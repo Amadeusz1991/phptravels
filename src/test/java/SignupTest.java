344d30125559f802bb731feeef7e5983739698dd
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-07-22 09:55:59 +0200

    Sighup flow using email generator with random umbers, and asserts checking out last name and entire heading

--- src/test/java/SignupTest.java
+++ src/test/java/SignupTest.java
@@ -20,8 +20,22 @@ public class SignupTest {
         driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         driver.get("http://www.kurs-selenium.pl/demo/");
 
+        // Zakładanie konta używając randomowego emaila
+        String lastName = "Jankowski";
+        int randomNumber = (int) (Math.random()*1000);
+        String email = "tester" + randomNumber + "@tester.pl";
         driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
         driver.findElements(By.xpath("//a[text()='  Sign Up']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElement(By.name("firstname")).sendKeys("Amadeusz");
+        driver.findElement(By.name("lastname")).sendKeys("Jankowski");
+        driver.findElement(By.name("phone")).sendKeys("546567890");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("Fasterekq");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Fasterekq");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
 
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(), "Hi, Amadeusz Jankowski");
     }
 }
