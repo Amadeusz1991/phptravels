3e123897def4f16db2b236ac95cab74197824458
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-07-23 09:36:05 +0200

    Creating object for results and changing path to search dynamic xpath text

--- src/test/java/com/selenium/demo/tests/HotelSearchTest.java
+++ src/test/java/com/selenium/demo/tests/HotelSearchTest.java
@@ -1,27 +1,26 @@
 package com.selenium.demo.tests;
 import com.selenium.demo.pages.HotelSearchPage;
-import org.openqa.selenium.By;
+import com.selenium.demo.pages.ResultsPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.List;
-import java.util.stream.Collectors;
 public class HotelSearchTest extends BaseTest {
 
     @Test
     public void searchHotel() {
 
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
-        hotelSearchPage.setCity("Dubai");
+        hotelSearchPage.setCity("Warsaw");
         hotelSearchPage.setDates("29/07/2021", "30/07/2021");
-        hotelSearchPage.setTravellers();
+        hotelSearchPage.setTravellers(1,2);
         hotelSearchPage.performSearch();
 
-        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b"))
-                .stream()
-                .map(el -> el.getAttribute("textContent"))
-                .collect(Collectors.toList());
+        // Odwołanie się do klasy ResultPage
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
         System.out.println(hotelNames.size());
         hotelNames.forEach(el -> System.out.println(el));
+
         //Asercje sprawdzające przwdziwość pobranych nazw hotelów
         Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
         Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
--- src/test/java/com/selenium/demo/tests/SearchWithNoResultTest.java
+++ src/test/java/com/selenium/demo/tests/SearchWithNoResultTest.java
@@ -1,29 +1,30 @@
 package com.selenium.demo.tests;
 
+import com.selenium.demo.pages.HotelSearchPage;
+import com.selenium.demo.pages.ResultsPage;
 import com.selenium.demo.tests.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.xml.transform.Result;
+
 public class SearchWithNoResultTest extends BaseTest {
 
     @Test
     public void searchWithNoResult() {
 
-        driver.findElement(By.cssSelector("input[name='checkin']")).sendKeys("17/08/2021");
-        driver.findElement(By.name("checkout")).sendKeys("24/08/2021");
-        driver.findElement(By.id("travellersInput")).click();
-        WebElement adult = driver.findElement(By.name("adults"));
-        adult.clear();
-        adult.sendKeys("3");
-        WebElement child = driver.findElement(By.name("child"));
-        child.clear();
-        child.sendKeys("2");
-        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setDates("17/08/2021","24/08/2021" );
+        hotelSearchPage.setTravellers(0, 1);
+        hotelSearchPage.performSearch();
+
+
         WebElement noResultHeading = driver.findElement(By.xpath("//h2[@class='text-center']"));
-        Assert.assertTrue(noResultHeading.isDisplayed());
-        Assert.assertEquals(noResultHeading.getText(), "No Results Found");
+        ResultsPage resultsPage = new ResultsPage(driver);
+        Assert.assertTrue(resultsPage.resultHeading.isDisplayed());
+        Assert.assertEquals(resultsPage.getHeadingText(), "No Results Found");
 
     }
 }
